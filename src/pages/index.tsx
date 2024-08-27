82e08af8e07bd8411511430ad3280aecb3a1a858
ziwei.hu <ziwei.hu@svca.cc>
2024-08-26 22:55:02 -0700

    fix compile error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,46 +13,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { useSession, } from "next-auth/react"
 
 // Components
-import PromptPanel from "../components/prompt-panel";
-import TileBoard from "../components/tile-board";
-import GameControls from "../components/game-controls";
-import Inventory from "../components/inventory";
 import Head from "next/head";
-import SignInRecommendation from "src/components/sign-in-recommendation";
 
 export default function Home() {
-//   const { status } = useSession();
-//   return (
-//     <>
-//       <Head>
-//         <title>Home | Developer Journey App</title>
-//       </Head>
-//       <main>
-//         {status === "authenticated" ? (
-//           <div className="grid grid-cols-1 sm:grid-cols-5 gap-3">
-//             <PromptPanel />
-//             <TileBoard />
-//             <GameControls />
-//             <Inventory />
-//           </div>
-//         ) : (<SignInRecommendation />)}
-//       </main>
-//     </>
-//   )
-    return {
+    return (
         <>
           <Head>
             <title>Home | Developer Journey App</title>
           </Head>
           <main>
-            <div>
+            <div style={{ textAlign: 'center' }}>
                 <h1>What GO0D do I do today?</h1>
-                <input type="button">Go GO0D</input>
+                <form>
+                    <input type="submit" id="good-button" name="Go GO0D"/>
+                </form>
             </div>
           </main>
         </>
-    }
+    )
 }
